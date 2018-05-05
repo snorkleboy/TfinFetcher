1c3bb2101a0738f30b359422b5e208609554b042
timkharshan@hotmail.com <timkharshan@hotmail.com>
2018-05-04 17:52:39 -0700

    alpha vantage save and fetch set up, not run yet, estimated 10hrs currently

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ mongoose.connect(uristring, {
         console.log('ERROR connecting to: ' + uristring + '. ' + err);
     } else {
         console.log('Succeeded connected to: ' + uristring);
-        // alphaVantageAnalytics();
+        alphaVantageAnalytics();
     }
 });
 
