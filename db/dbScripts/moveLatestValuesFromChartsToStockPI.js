8abf171bba1cd6de2203e801530b9d1f01e166ea
timkharshan@hotmail.com <timkharshan@hotmail.com>
2018-05-28 15:23:16 -0700

    made a more controllable init pattern

--- db/dbScripts/moveLatestValuesFromChartsToStockPI.js
+++ db/dbScripts/moveLatestValuesFromChartsToStockPI.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const promiseIterator = require('../util/generalPromiseIterator.js')
 // const promiseFiller = require('../util/promiseFiller.js')
 
-function moveValuesFromChartsToStocks(batchSize = 20, startI = 0, stopAt) {
+function moveValuesFromChartsToStocks(batchSize = 20, startI = 3000, stopAt) {
     console.log("moving most recent stockchart info into each stock ")
     return promiseIterator(
         StockChart,
