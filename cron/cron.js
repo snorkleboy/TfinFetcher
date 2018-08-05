af9ccf277858e0490e56b971beee4546ff7ec36d
artem <timkharshan@hotmail.com>
2018-08-05 16:34:10 -0700

    took run out of cron job

--- cron/cron.js
+++ cron/cron.js
@@ -4,7 +4,6 @@ const interface = new StocksInterface();
 
 module.exports = function setupCron(){
     console.log("setting up cron job")
-    interface.run();
 
     cron.schedule("0 15 * * 1,2,3,4,5", function() {
     console.log("running update", )
