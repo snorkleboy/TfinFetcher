c1a0c4bfcc7fcc471890367220051d36150d3f19
timkharshan@hotmail.com <timkharshan@hotmail.com>
2018-08-05 18:10:47 -0700

    quick fix

--- cron/cron.js
+++ cron/cron.js
@@ -4,13 +4,19 @@ const interface = new StocksInterface();
 
 module.exports = function setupCron(){
     console.log("setting up cron job")
-
+            interface.checkTimeAndConnection()
+                .then(() => {
+                    interface.run();
+                })
     cron.schedule("0 15 * * 1,2,3,4,5", function() {
     console.log("running update", )
-        checkTimeAndConnection();
-        setTimeout(()=>{
-            interface.run();
-        },1000*60*15)
+        interface.checkTimeAndConnection()
+        .then(()=>{
+            setTimeout(() => {
+                interface.run();
+            }, 1000 * 60 * 15)
+        })
+
 
     });
 
